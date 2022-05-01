8081462cb50f7e70f7af8c030a5fbabe5c87531a
Vinicius Barbosa de Medeiros <bmvinicius11@gmail.com>
2022-05-01 02:37:43 -0300

    feat(gateway): add fastify basic server

--- gateway/src/application/app.ts
+++ gateway/src/application/app.ts
@@ -1,1 +1,9 @@
-console.log('Hello World')
+import fastify from 'fastify'
+
+const app = fastify({ logger: true })
+
+app.get('/health', async (req, res) => {
+  return { alive: true }
+})
+
+export default app
