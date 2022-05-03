6d87513cee2db832ecb04e309312e428536024fb
Vinicius Barbosa de Medeiros <bmvinicius11@gmail.com>
2022-05-03 01:11:29 -0300

    refactor(gateway): break card payment api models

--- gateway/src/adapters/card/httpCardPaymentAPI.ts
+++ gateway/src/adapters/card/httpCardPaymentAPI.ts
@@ -1,11 +1,11 @@
 import axios from 'axios'
 
-import { CardPayment, CardPaymentAPI, Payment } from '../../core/cardPayment/cardPayment'
+import { CardPaymentAPI, CardPaymentAPIRequest, CardPaymentAPIResponse } from '../../core/cardPayment/cardPayment'
 
 export default class HttpCardPaymentAPI implements CardPaymentAPI {
-  async process (cardPayment: CardPayment): Promise<Payment> {
+  async process (cardPayment: CardPaymentAPIRequest): Promise<CardPaymentAPIResponse> {
     try {
-      const { data } = await axios.post<Payment>('https://run.mocky.io/v3/c6c36610-b5a4-4833-97dc-b53a7083de59', cardPayment)
+      const { data } = await axios.post<CardPaymentAPIResponse>('https://run.mocky.io/v3/c6c36610-b5a4-4833-97dc-b53a7083de59', cardPayment)
       return data
     } catch (error) {
       console.log(error)
--- gateway/src/core/cardPayment/cardPayment.ts
+++ gateway/src/core/cardPayment/cardPayment.ts
@@ -14,7 +14,28 @@ export type CardPayment = {
   }
 }
 
-export type Payment = {
+export type CardPaymentResponse = {
+  id: string
+  clientId: string
+  amount: PaymentAmount
+  paymentType: PaymentType
+}
+
+export type CardPaymentAPIRequest = {
+  id: string
+  clientId: string
+  amount: PaymentAmount
+  paymentType: PaymentType
+  cardPaymentInfo: {
+    number: string
+    cvv: string
+    expirationMonth: string
+    expirationYear: string
+    cardholderName: string
+  }
+}
+
+export type CardPaymentAPIResponse = {
   id: string
   clientId: string
   amount: PaymentAmount
@@ -22,17 +43,17 @@ export type Payment = {
 }
 
 export interface CardProcessor {
-  processCardPayment(cardPayment: CardPayment): Promise<Payment>
+  processCardPayment(cardPayment: CardPayment): Promise<CardPaymentResponse>
 }
 
 export interface CardPaymentAPI {
-  process(cardPayment: CardPayment): Promise<Payment>
+  process(cardPayment: CardPaymentAPIRequest): Promise<CardPaymentAPIResponse>
 }
 
 export default class CardPaymentProcessor implements CardProcessor {
   constructor (private readonly cardPaymentAPI: CardPaymentAPI) {}
 
-  async processCardPayment (cardPayment: CardPayment): Promise<Payment> {
+  processCardPayment (cardPayment: CardPayment): Promise<CardPaymentResponse> {
     return this.cardPaymentAPI.process(cardPayment)
   }
 }
