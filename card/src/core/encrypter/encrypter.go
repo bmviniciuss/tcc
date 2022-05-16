82fef0fb2e170da0e84c90c3af49558c049cafac
Vinicius Barbosa de Medeiros <bmvinicius11@gmail.com>
2022-05-16 20:27:18 -0300

    feat: add descrypt implementation

--- card/src/core/encrypter/encrypter.go
+++ card/src/core/encrypter/encrypter.go
@@ -6,10 +6,12 @@ import (
 	"crypto/cipher"
 	b64 "encoding/base64"
 	"os"
+	"strings"
 )
 
 type Encrypter interface {
 	Encrypt(content []byte) (encryptedContent []byte, err error)
+	Decrypt(encryptedData []byte) (decryptedContent []byte, err error)
 }
 
 type encryter struct {
@@ -23,7 +25,7 @@ func NewEncrypter(encryptionKey []byte) *encryter {
 }
 
 func (e *encryter) Encrypt(content []byte) (encryptedContent []byte, err error) {
-	plainText := PKCS5Padding(content, aes.BlockSize)
+	plainText := pKCS5Padding(content, aes.BlockSize)
 	ciphertext := make([]byte, len(plainText))
 
 	block, err := aes.NewCipher(e.encryptionKey)
@@ -39,7 +41,27 @@ func (e *encryter) Encrypt(content []byte) (encryptedContent []byte, err error) 
 	return []byte(encoded), nil
 }
 
-func PKCS5Padding(cipherText []byte, blockSize int) []byte {
+func (e *encryter) Decrypt(encryptedData []byte) (decryptedContent []byte, err error) {
+	iv := []byte(os.Getenv("ENCRYPTION_IV"))
+	decodedData, err := b64.StdEncoding.DecodeString(string(encryptedData))
+
+	if err != nil {
+		return nil, err
+	}
+
+	block, err := aes.NewCipher(e.encryptionKey)
+	if err != nil {
+		return nil, err
+	}
+
+	mode := cipher.NewCBCDecrypter(block, iv)
+	mode.CryptBlocks(decodedData, decodedData)
+
+	cutTrailingSpaces := []byte(strings.TrimSpace(string(decodedData)))
+	return cutTrailingSpaces, err
+}
+
+func pKCS5Padding(cipherText []byte, blockSize int) []byte {
 	padding := blockSize - len(cipherText)%blockSize
 	padText := bytes.Repeat([]byte{byte(padding)}, padding)
 	return append(cipherText, padText...)
