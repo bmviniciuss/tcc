6d415c0d6c23a117ee944dd782e042872b79760b
Vinicius Barbosa de Medeiros <bmvinicius11@gmail.com>
2022-05-18 22:05:45 -0300

    chore(gateway): add start logging

--- gateway/src/application/index.ts
+++ gateway/src/application/index.ts
@@ -4,6 +4,7 @@ import { ENV } from './config/env'
 async function main (): Promise<void> {
   try {
     await app.listen(ENV.PORT)
+    console.log('Server listening on port: ', ENV.PORT)
   } catch (error) {
     app.log.error(error)
     process.exit(1)
