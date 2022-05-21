0189e885cdb34a554036bc7c186558576dea04e3
Vinicius Barbosa de Medeiros <bmvinicius11@gmail.com>
2022-05-21 02:01:50 -0300

    feat(card-payment): add field validation

--- card-payment/go.mod
+++ card-payment/go.mod
@@ -4,7 +4,14 @@ go 1.18
 
 require (
 	github.com/go-chi/chi/v5 v5.0.7 // indirect
+	github.com/go-playground/locales v0.14.0 // indirect
+	github.com/go-playground/universal-translator v0.18.0 // indirect
+	github.com/go-playground/validator/v10 v10.11.0 // indirect
 	github.com/jmoiron/sqlx v1.3.5 // indirect
 	github.com/joho/godotenv v1.4.0 // indirect
+	github.com/leodido/go-urn v1.2.1 // indirect
 	github.com/lib/pq v1.10.6 // indirect
+	golang.org/x/crypto v0.0.0-20220518034528-6f7dac969898 // indirect
+	golang.org/x/sys v0.0.0-20220520151302-bc2c85ada10a // indirect
+	golang.org/x/text v0.3.7 // indirect
 )
--- card-payment/src/http/handlers/payment.go
+++ card-payment/src/http/handlers/payment.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/bmviniciuss/tcc/card-payment/src/core/payment"
 	"github.com/go-chi/chi/v5"
+	"github.com/go-playground/validator/v10"
 )
 
 type PaymentController struct {
@@ -24,15 +25,15 @@ func (p PaymentController) Route(r chi.Router) {
 }
 
 type CardPaymentRequest struct {
-	ClientId    string      `json:"client_id"`
-	Amount      float64     `json:"amount"`
-	PaymentType string      `json:"payment_type"`
-	PaymentDate time.Time   `json:"payment_date"`
-	PaymentInfo PaymentInfo `json:"payment_info"`
+	ClientId    string      `json:"client_id" validate:"required,uuid4"`
+	Amount      float64     `json:"amount" validate:"required"`
+	PaymentType string      `json:"payment_type" validate:"required"`
+	PaymentDate time.Time   `json:"payment_date" validate:"required"`
+	PaymentInfo PaymentInfo `json:"payment_info" validate:"required"`
 }
 
 type PaymentInfo struct {
-	CardToken string `json:"card_token"`
+	CardToken string `json:"card_token" validate:"required"`
 }
 
 type CardPaymentResponse struct {
@@ -59,6 +60,17 @@ func handleProcessPayment(paymentService payment.Service) func(w http.ResponseWr
 			return
 		}
 
+		validate := validator.New()
+
+		if err := validate.Struct(cardPaymentRequest); err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			json.NewEncoder(w).Encode(map[string]interface{}{
+				"error":   true,
+				"message": err.Error(),
+			})
+			return
+		}
+
 		input := payment.ProcessPaymentInput{
 			ClientId:    cardPaymentRequest.ClientId,
 			Amount:      cardPaymentRequest.Amount,
@@ -72,6 +84,7 @@ func handleProcessPayment(paymentService payment.Service) func(w http.ResponseWr
 		payment, err := paymentService.Process(&input)
 
 		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
 			json.NewEncoder(w).Encode(map[string]interface{}{
 				"error":   true,
 				"message": err.Error(),
