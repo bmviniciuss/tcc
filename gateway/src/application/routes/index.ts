86d23b3ec46911616ddd84dd2827f664a56c9dde
Vinicius Barbosa de Medeiros <bmvinicius11@gmail.com>
2022-05-21 02:37:57 -0300

    feat(gateway): add card payment gateway route for http service

--- gateway/src/application/routes/index.ts
+++ gateway/src/application/routes/index.ts
@@ -1,6 +1,7 @@
 import { FastifyInstance } from 'fastify'
 
 import cardsRoutes from './cards'
+import paymentRoutes from './payment'
 
 export default async function routes (fastify: FastifyInstance) {
   fastify.get('/health', async () => {
@@ -8,4 +9,5 @@ export default async function routes (fastify: FastifyInstance) {
   })
 
   fastify.register(cardsRoutes, { prefix: 'cards' })
+  fastify.register(paymentRoutes, { prefix: 'payments' })
 }
