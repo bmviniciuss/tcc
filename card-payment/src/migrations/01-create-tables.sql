fa28041bc7241b48651844fbffef129d7aaa1db9
Vinicius Barbosa de Medeiros <bmvinicius11@gmail.com>
2022-05-21 01:47:57 -0300

    feat(card-payment): add migration to docker-compose

--- card-payment/src/migrations/01-create-tables.sql
+++ card-payment/src/migrations/01-create-tables.sql
@@ -11,7 +11,7 @@ CREATE TABLE IF NOT EXISTS payments (
   payment_date TIMESTAMPTZ not null,
   created_at TIMESTAMPTZ default now(),
   constraint cards_pkey primary key (id)
-)
+);
 
 create table if not exists payables (
   id uuid not null default uuid_generate_v4(),
@@ -22,4 +22,4 @@ create table if not exists payables (
   created_at TIMESTAMPTZ default now(),
   constraint payables_pkey primary key (id),
   constraint payables_payment_id_fkey foreign key (payment_id) references payments(id)
-)
+);
