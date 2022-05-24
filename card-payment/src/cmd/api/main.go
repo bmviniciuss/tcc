be41bf9949a50a29835563ee03654cbaa2faf60b
Vinicius Barbosa de Medeiros <bmvinicius11@gmail.com>
2022-05-23 21:07:43 -0300

    feat(card-payment): add grpc server

--- card-payment/src/cmd/api/main.go
+++ card-payment/src/cmd/api/main.go
@@ -1,13 +1,21 @@
 package main
 
 import (
+	"fmt"
 	"log"
+	"net"
 	"net/http"
 	"os"
 
 	"github.com/bmviniciuss/tcc/card-payment/src/adapters/db"
+	"github.com/bmviniciuss/tcc/card-payment/src/factories"
+	grpcpaymentserver "github.com/bmviniciuss/tcc/card-payment/src/grpc"
+	"github.com/bmviniciuss/tcc/card-payment/src/grpc/pb"
 	api "github.com/bmviniciuss/tcc/card-payment/src/http"
+	"github.com/jmoiron/sqlx"
 	"github.com/joho/godotenv"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
 )
 
 func main() {
@@ -18,8 +26,36 @@ func main() {
 	}
 
 	db := db.ConnectDB()
-
 	appPort := os.Getenv("PORT")
+	grpcEnabled := os.Getenv("GRPC_ENABLED")
+
+	if grpcEnabled == "true" {
+		runGrpc(db, appPort)
+	} else {
+		runHttp(db, appPort)
+	}
+}
+
+func runGrpc(db *sqlx.DB, appPort string) {
+	gs := grpc.NewServer()
+	paymentService := factories.NewPaymentService(db)
+	pb.RegisterCardPaymentServer(gs, grpcpaymentserver.NewCardPaymentServer(paymentService))
+	reflection.Register(gs)
+
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%s", appPort))
+
+	if err != nil {
+		log.Fatal("[gRPC] Server closed unexpected", err.Error())
+	}
+
+	log.Printf("[gRPC] Server started on port: %s\n", appPort)
+
+	if err = gs.Serve(lis); err != nil {
+		log.Fatal("[gRPC] Server closed unexpected")
+	}
+}
+
+func runHttp(db *sqlx.DB, appPort string) {
 
 	mux := api.NewApi(db)
 	server := http.Server{
@@ -27,9 +63,8 @@ func main() {
 		Handler: mux,
 	}
 
-	log.Println("Server started on port " + appPort)
+	log.Println("[HTTP] Server started on port " + appPort)
 	if err := server.ListenAndServe(); err != http.ErrServerClosed {
-		log.Fatal("Server closed unexpected")
+		log.Fatal("[HTTP] Server closed unexpected")
 	}
-
 }
---
+++ card-payment/src/grpc/grpcPaymentServer.go
@@ -0,0 +1,53 @@
+package grpcpaymentserver
+
+import (
+	"context"
+	"time"
+
+	"github.com/bmviniciuss/tcc/card-payment/src/core/payment"
+	"github.com/bmviniciuss/tcc/card-payment/src/grpc/pb"
+)
+
+type CardPaymentServer struct {
+	pb.UnimplementedCardPaymentServer
+	PaymentService payment.Service
+}
+
+func NewCardPaymentServer(service payment.Service) *CardPaymentServer {
+	return &CardPaymentServer{
+		PaymentService: service,
+	}
+}
+
+func (s *CardPaymentServer) ProccessCardPayment(ctx context.Context, in *pb.ProcessCardPaymentInput) (*pb.Payment, error) {
+	t, err := time.Parse(time.RFC3339, in.GetPaymentDate())
+	if err != nil {
+		return nil, err
+	}
+	p := int32(in.GetPaymentType())
+	input := &payment.ProcessPaymentInput{
+		ClientId:    in.GetClientId(),
+		Amount:      float64(in.GetAmount()),
+		PaymentType: pb.PaymentTypeEnum_name[p],
+		PaymentDate: t,
+		PaymentInfo: payment.PaymentInfoInput{
+			CardToken: in.GetPaymentInfo().GetCardToken(),
+		},
+	}
+	payment, err := s.PaymentService.Process(input)
+	if err != nil {
+		return nil, err
+	}
+
+	return &pb.Payment{
+		Id:          payment.Id,
+		ClientId:    payment.ClientId,
+		Amount:      float32(payment.Amount),
+		PaymentType: payment.PaymentType,
+		PaymentDate: payment.PaymentDate.Format(time.RFC3339),
+		PaymentInfo: &pb.PaymentInfo{
+			MaskedNumber: payment.PaymentInfo.MaskedNumber,
+		},
+	}, nil
+
+}
