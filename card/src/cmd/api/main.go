58b4535894c56031b61ee5d3396ebd0d4542b220
Vinicius Barbosa de Medeiros <bmvinicius11@gmail.com>
2022-05-25 02:59:33 -0300

    refactor(card): add logging to main

--- card/src/cmd/api/main.go
+++ card/src/cmd/api/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net"
 	"net/http"
@@ -10,6 +11,7 @@ import (
 	grpccard "github.com/bmviniciuss/tcc/card/src/grpc"
 	"github.com/bmviniciuss/tcc/card/src/grpc/pb"
 	api "github.com/bmviniciuss/tcc/card/src/http"
+	"github.com/jmoiron/sqlx"
 
 	"github.com/joho/godotenv"
 	"google.golang.org/grpc"
@@ -20,41 +22,51 @@ func main() {
 	err := godotenv.Load()
 
 	if err != nil {
-		log.Fatal("Error loading .env file")
+		log.Fatal("[Main] Error loading .env file")
 	}
 
 	db := db.ConnectDB()
-
 	grpcEnabled := os.Getenv("GRPC_ENABLED")
 
 	if grpcEnabled == "true" {
-		grpcPort := os.Getenv("PORT")
-		grpcServer := grpc.NewServer()
-		pb.RegisterCardsServer(grpcServer, grpccard.NewCardServiceServer(db))
-		reflection.Register(grpcServer)
-
-		lis, err := net.Listen("tcp", ":"+grpcPort)
-
-		if err != nil {
-			log.Fatal("Server closed unexpected", err.Error())
-		}
-
-		log.Println("GRPC server started on port: " + grpcPort)
-		if err = grpcServer.Serve(lis); err != nil {
-			log.Fatal("Server closed unexpected")
-		}
+		runGRPC(db)
 	} else {
-		appPort := os.Getenv("PORT")
-		mux := api.NewApi(db)
+		runHTTP(db)
+	}
+}
 
-		server := http.Server{
-			Addr:    ":" + appPort,
-			Handler: mux,
-		}
+func runGRPC(db *sqlx.DB) {
+	log.Println("[gRPC] Starting gRPC server...")
 
-		log.Println("Server started on port " + appPort)
-		if err := server.ListenAndServe(); err != http.ErrServerClosed {
-			log.Fatal("Server closed unexpected")
-		}
+	grpcPort := os.Getenv("PORT")
+	grpcServer := grpc.NewServer()
+	pb.RegisterCardsServer(grpcServer, grpccard.NewCardServiceServer(db))
+	reflection.Register(grpcServer)
+	lis, err := net.Listen("tcp", ":"+grpcPort)
+
+	if err != nil {
+		log.Fatal("[gRPC] Server closed unexpected", err.Error())
+	}
+
+	log.Println("[gRPC] Server started on port: " + grpcPort)
+	if err = grpcServer.Serve(lis); err != nil {
+		log.Fatal("[gRPC] Server closed unexpected")
+	}
+}
+
+func runHTTP(db *sqlx.DB) {
+	log.Println("[HTTP] Starting HTTP server...")
+
+	appPort := os.Getenv("PORT")
+	mux := api.NewApi(db)
+
+	server := http.Server{
+		Addr:    fmt.Sprintf(":%s", appPort),
+		Handler: mux,
+	}
+
+	log.Println("[HTTP] Server started on port " + appPort)
+	if err := server.ListenAndServe(); err != http.ErrServerClosed {
+		log.Fatal("[HTTP] Server closed unexpected")
 	}
 }
