b5e9dce040eb003d4ec89fa5de109df0157970e7
Vinicius Barbosa de Medeiros <bmvinicius11@gmail.com>
2022-06-11 11:30:56 -0300

    refactor: extract mappers and request schema to their own files

---
+++ client-wallet/src/application/http/routes/transactionsRoutes.ts
@@ -0,0 +1,35 @@
+import { PrismaClient } from '@prisma/client'
+import { FastifyPluginCallback } from 'fastify'
+import { FromSchema } from 'json-schema-to-ts'
+
+import PrismaTransactionRepository from '../../../adapters/transaction/PrismaTransactionRepository'
+import TransactionService from '../../../core/transaction/TransactionService'
+import { CreateTransactionInput } from '../../../core/transaction/transaction.interfaces'
+import logger from '../../../utils/logger'
+import { TransactionRequestSchema } from './schemas/transaction'
+import PresentationTransactionMapper from '../mappers/PresentationTransactionMapper'
+
+export const transactionsRoutes = (prisma: PrismaClient): FastifyPluginCallback => {
+  return async (fastify) => {
+    const l = logger.child({ label: transactionsRoutes.name })
+
+    fastify.post<{ Body: FromSchema<typeof TransactionRequestSchema>}>('/', {
+      schema: {
+        body: TransactionRequestSchema
+      }
+    }, async (request, reply) => {
+      try {
+        l.info('Received transaction request')
+        const prismaTransactionRepository = new PrismaTransactionRepository(prisma)
+        const service = new TransactionService(prismaTransactionRepository)
+        const createTransactionInput: CreateTransactionInput = PresentationTransactionMapper.mapRequestInputToCreateTransactionInput(request.body)
+        const transaction = await service.create(createTransactionInput)
+        const presentationTransaction = PresentationTransactionMapper.mapTransactionToPresentation(transaction)
+        return reply.status(201).send(presentationTransaction)
+      } catch (error: any) {
+        l.error('Error creating transaction', error)
+        return reply.status(500).send({ error: error?.message })
+      }
+    })
+  }
+}
