7228845299a75bd0f149a82b7f927db42e00bdcd
Vinicius Barbosa de Medeiros <bmvinicius11@gmail.com>
2022-06-21 21:24:10 -0300

    feat(card-payment): add grpc client wallet api

--- card-payment/src/factories/paymentservice.go
+++ card-payment/src/factories/paymentservice.go
@@ -6,6 +6,9 @@ import (
 
 	grpccardapi "github.com/bmviniciuss/tcc/card-payment/src/adapters/card/grpc"
 	httpcardapi "github.com/bmviniciuss/tcc/card-payment/src/adapters/card/http"
+	grpcclientwallet "github.com/bmviniciuss/tcc/card-payment/src/adapters/client_wallet/grpc"
+	httpclientwalletapi "github.com/bmviniciuss/tcc/card-payment/src/adapters/client_wallet/http"
+
 	postgrespaymentrepository "github.com/bmviniciuss/tcc/card-payment/src/adapters/payment"
 	"github.com/bmviniciuss/tcc/card-payment/src/core/payment"
 	"github.com/jmoiron/sqlx"
@@ -33,6 +36,24 @@ func NewCardApi() payment.CardAPI {
 
 }
 
+func NewClientWalletAPI() payment.ClientWalletTransactionAPI {
+	log.Println("Creating ClientWalletAPI based on the GRPC_ENABLED environment variable")
+	e := os.Getenv("GRPC_ENABLED")
+	if e == "true" {
+		log.Println("gRPC client wallet API is not implemented yet")
+		host := os.Getenv("CLIENT_WALLET_HOST")
+		grpcConn, err := grpc.Dial(host, grpc.WithTransportCredentials(insecure.NewCredentials()))
+
+		if err != nil {
+			log.Fatal("Error connecting to CLIENT WALLET gRPC server")
+		}
+
+		return grpcclientwallet.NewGRPCClientWalletAPI(grpcConn)
+	}
+	log.Println("Creating a HTTP client wallet API")
+	return httpclientwalletapi.NewHTTPClientWalletTransactionAPI()
+}
+
 func NewPaymentService(db *sqlx.DB) payment.Service {
-	return payment.NewPaymentService(NewCardApi(), postgrespaymentrepository.NewPostgresPaymentRepository(db))
+	return payment.NewPaymentService(NewCardApi(), postgrespaymentrepository.NewPostgresPaymentRepository(db), NewClientWalletAPI())
 }
