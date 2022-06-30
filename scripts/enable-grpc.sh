d6bcfb877eb41c6a0fcf166b805717265cffa9d4
Vinicius Barbosa de Medeiros <bmvinicius11@gmail.com>
2022-06-30 11:17:53 -0300

    refactor: remove sh scripts in favor of tcc cli

--- scripts/enable-grpc.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-node ./env-modifier/index.js ../card/.env true
-node ./env-modifier/index.js ../card-payment/.env true
-node ./env-modifier/index.js ../client-wallet/.env true
-node ./env-modifier/index.js ../gateway/.env true
--- scripts/enable-http.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-node ./env-modifier/index.js ../card/.env false
-node ./env-modifier/index.js ../card-payment/.env false
-node ./env-modifier/index.js ../client-wallet/.env false
-node ./env-modifier/index.js ../gateway/.env false
