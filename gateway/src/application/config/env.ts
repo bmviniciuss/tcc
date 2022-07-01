238d635ff566502c622c63cefab693bf1eeeabbf
Vinicius Barbosa de Medeiros <bmvinicius11@gmail.com>
2022-07-01 01:11:35 -0300

    refactor: remove node gateway

--- gateway/src/application/config/env.ts
+++
@@ -1,9 +0,0 @@
-import 'dotenv/config'
-
-export const ENV = {
-  PORT: Number(process.env?.PORT ?? '4000'),
-  ENABLE_GRPC: process.env?.GRPC_ENABLED === 'true',
-  CARD_HOST: process.env?.CARD_HOST ?? 'localhost:5001',
-  CARD_PAYMENT_HOST: process.env?.CARD_PAYMENT_GRPC_HOST ?? 'localhost:5555',
-  CLIENT_WALLET_HOST: process.env?.CLIENT_WALLET_HOST ?? 'localhost:5004'
-}
