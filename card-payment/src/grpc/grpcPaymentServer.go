f419ebf7d98f50320b2626be58219992456c66cc
Vinicius Barbosa de Medeiros <bmvinicius11@gmail.com>
2022-07-01 23:20:59 -0300

    feat(card-payment): add grpc method to get clients payments

--- card-payment/src/grpc/grpcPaymentServer.go
+++ card-payment/src/grpc/grpcPaymentServer.go
@@ -6,6 +6,8 @@ import (
 
 	"github.com/bmviniciuss/tcc/card-payment/src/core/payment"
 	"github.com/bmviniciuss/tcc/card-payment/src/grpc/pb"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
 
 type CardPaymentServer struct {
@@ -50,4 +52,42 @@ func (s *CardPaymentServer) ProccessCardPayment(ctx context.Context, in *pb.Proc
 		},
 	}, nil
 
+}
+
+func (s *CardPaymentServer) GetPaymentsByClientId(ctx context.Context, in *pb.GetPaymentsByClientIdRequest) (*pb.PaymentsResults, error) {
+	id := in.GetClientId()
+	if id == "" {
+		return nil, status.Errorf(codes.InvalidArgument, "The field 'client_id' must be provided.")
+	}
+
+	input := &payment.GetPaymentsByClientIdInput{
+		ClientId: in.GetClientId(),
+	}
+
+	pp, err := s.PaymentService.GetPaymentsByClientId(input)
+
+	if err != nil {
+		return nil, status.Errorf(codes.Internal, "An error occur while searching for the payments")
+	}
+
+	c := []*pb.Payment{}
+	for _, payment := range pp {
+		p := &pb.Payment{
+			Id:          payment.Id,
+			ClientId:    payment.ClientId,
+			PaymentType: payment.PaymentType,
+			PaymentDate: payment.PaymentDate.Format(time.RFC3339),
+			Amount:      payment.Amount,
+			PaymentInfo: &pb.PaymentInfo{
+				MaskedNumber: payment.PaymentInfo.MaskedNumber,
+			},
+		}
+		c = append(c, p)
+	}
+
+	res := pb.PaymentsResults{
+		Content: c,
+	}
+
+	return &res, nil
 }
