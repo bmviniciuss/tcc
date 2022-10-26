2943131d2fd28c1e2a7afe9f4c80fb4474613303
Vinicius Barbosa de Medeiros <bmvinicius11@gmail.com>
2022-10-26 15:17:04 -0300

    feat: add run all test script

--- benchmarks/benchmarks/run-all.sh
+++ benchmarks/benchmarks/run-all.sh
@@ -6,6 +6,13 @@ do
     for i in {1..1}
     do
         echo "$f:$GRPC_ENABLED"
+        GRPC_ENABLED=$GRPC_ENABLED docker compose -f ../../docker-compose.yml up -d
+        sleep 5
         k6 run $f -e GENERATE_SUMMARY=true -e GRPC_ENABLED=$GRPC_ENABLED
+        sleep 5
+        docker compose -f ../../docker-compose.yml down -v
+        sleep 3
     done
+
+    docker compose -f ../../docker-compose.yml down -v
 done
